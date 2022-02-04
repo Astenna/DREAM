4659e0f5327a9e99f5ae09a2d79626d7767eadae
Józef Piechaczek <244761@student.pwr.edu.pl>
2022-02-04 20:30:30 +0100

    Persist frontend state

--- Code/Client/src/store/store.ts
+++ Code/Client/src/store/store.ts
@@ -1,14 +1,49 @@
-import {configureStore} from '@reduxjs/toolkit'
+import {combineReducers, createStore} from '@reduxjs/toolkit'
 import authReducer from './auth/authSlice'
+
+/**
+ * Redux reducers combined.
+ */
+const reducers = combineReducers({
+  auth: authReducer,
+})
+
+/**
+ * Saves redux state to local storage.
+ * @param state
+ */
+const saveToLocalStorage = (state: any) => {
+  try {
+    console.log("eeee")
+    const serializedState = JSON.stringify(state);
+    localStorage.setItem('state', serializedState);
+  } catch (e) {
+
+  }
+}
+
+/**
+ * Reads redux state from local storage.
+ * @returns {any|undefined}
+ */
+const loadFromLocalStorage = () => {
+  try {
+    const serializedState = localStorage.getItem('state');
+    if (serializedState === null) return undefined;
+    return JSON.parse(serializedState);
+  } catch (e) {
+    return undefined;
+  }
+}
 
 /**
  * Application store. Contains global data of the application.
  */
-const store = configureStore({
-  reducer: {
-    auth: authReducer
-  },
-})
+const store = createStore(
+  reducers,
+  loadFromLocalStorage()
+)
+store.subscribe(() => saveToLocalStorage(store.getState()))
 
 export default store
 export type RootState = ReturnType<typeof store.getState>
